ac2bd8b0a10545e8c08339bd2910aa9c43783ab2
aviadb <aviadb77@gmail.com>
2026-05-31 15:32:38 +0300

    update surfer exec in path and verilator

--- load_env.sh
+++ load_env.sh
@@ -1,3 +1,3 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 echo $SCRIPT_DIR
-export PATH=${SCRIPT_DIR}/third-party/verilator/bin:$PATH
+export PATH=${SCRIPT_DIR}:${SCRIPT_DIR}/third-party/verilator/bin:$PATH
