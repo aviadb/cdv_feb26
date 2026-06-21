0cb685867ddb7c01067249b6cd8c269062d24996
aviadb <aviadb77@gmail.com>
2026-06-21 12:47:45 +0300

    Update deps

--- load_env.sh
+++ load_env.sh
@@ -1,3 +1,3 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 echo $SCRIPT_DIR
-export PATH=${SCRIPT_DIR}:${SCRIPT_DIR}/third-party/verilator/bin:${SCRIPT_DIR}/third-party/bin:$PATH
+export PATH=${SCRIPT_DIR}:${SCRIPT_DIR}/third-party/bin:${SCRIPT_DIR}/third-party/verilator/bin:${SCRIPT_DIR}/third-party/bin:$PATH
